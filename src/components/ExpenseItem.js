f5a4eb6da6397fabc7a8bb8b6221c5426dbb656e
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2023-05-27 11:05:43 +0530

    expense_item

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+
+const ExpenseItem = (props) => {
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
+
+    return (
+        <div className="expense-item">
+            <ExpenseDate date = {props.expense.date}></ExpenseDate>
+            <div className="expense-item__description">
+                <h2>{props.expense.title}</h2>
+                <div className="expense-item__price">${props.expense.amount}</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
