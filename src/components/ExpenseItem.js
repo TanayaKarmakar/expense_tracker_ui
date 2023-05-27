a5ac30f8477461bc31eed28d81914c7c0306ff22
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2023-05-27 20:51:34 +0530

    code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,6 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 
 const ExpenseItem = (props) => {
     const expenseDate = new Date(2021, 2, 28);
@@ -7,13 +8,13 @@ const ExpenseItem = (props) => {
     const expenseAmount = 294.67;
 
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date = {props.expense.date}></ExpenseDate>
             <div className="expense-item__description">
                 <h2>{props.expense.title}</h2>
                 <div className="expense-item__price">${props.expense.amount}</div>
             </div>
-        </div>
+        </Card>
     );
 }
 
